7148a0ca0d621b9844a282c0468aad33719d3e61
luasenvy <luas.envy@gmail.com>
2021-02-09 17:35:10 +0900

    improve code

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 ```
-git clone https://github.com/luasenvy/rapidfire-example-mongodb
-cd rapidfire-example-mongodb
+git clone https://github.com/luasenvy/rapidfire-example-nuxt
+cd rapidfire-example-nuxt
 npm i
 npm start
 ```
--- src/middlewares/NuxtMiddleware.js
+++ src/middlewares/NuxtMiddleware.js
@@ -9,11 +9,11 @@ const {
 class NuxtMiddleware extends Middleware {
   constructor() {
     super()
+    this.nuxt = null
   }
 
   async init() {
     this.nuxt = new Nuxt({ telemetry: false, srcDir: path.join(this.$rapidfire.env.paths.root, 'src/nuxt') })
-
     await this.nuxt.ready()
 
     // Build only in dev mode
