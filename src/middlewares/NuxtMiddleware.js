83de8e184e9e5a248eb1d61a142a3e4e79b398eb
luasenvy <luas.envy@gmail.com>
2021-02-11 23:31:24 +0900

    improve code

--- src/middlewares/NuxtMiddleware.js
+++ src/middlewares/NuxtMiddleware.js
@@ -9,22 +9,24 @@ const {
 class NuxtMiddleware extends Middleware {
   constructor() {
     super()
+
     this.nuxt = null
   }
 
   async init() {
-    this.nuxt = new Nuxt({ telemetry: false, srcDir: path.join(this.$rapidfire.env.paths.root, 'src/nuxt') })
+    const isDev = this.$rapidfire.options.isDev
+
+    this.nuxt = new Nuxt({ telemetry: false, dev: isDev, srcDir: path.join(this.$rapidfire.env.paths.root, 'src/nuxt/') })
+
     await this.nuxt.ready()
 
     // Build only in dev mode
-    if (this.isDev) {
+    if (isDev) {
       const builder = new Builder(this.nuxt)
       await builder.build()
     }
-  }
 
-  pipe(req, res, next) {
-    this.nuxt.render(req, res, next)
+    this.pipelines.push({ pipe: this.nuxt.render })
   }
 }
 
